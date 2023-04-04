eb144cdfbaf65d0241b6c6c7838b8566ef11ac06
DaniekTj <65110807+daniektj@users.noreply.github.com>
2023-04-04 05:55:34 -0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/03t02NU3l0Xy8mDs/scene.splinecode');
